a26aae5c7f8c914fff0ac4527c1824a72f4f64af
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-23 16:21:14 +0300

    opened lastSatation

---
+++ Mamaosem/MamaOsem/HouseCakeLine/LastStation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mamaosem.Utils;
+
+namespace MamaOsem.CakeLine
+{
+    public class LastStation : HouseCakeLineSatationChainBase
+    {
+        public override bool Act(HouseCake cake)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
