dc88a32cd23e6abf126a06e793dc1a519f6ac9ab
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-23 12:03:56 +0300

    added chainBase

---
+++ Mamaosem/MamaOsem/HouseCakeLine/HouseCakeLineSatationChainBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mamaosem.Utils;
+
+namespace MamaOsem.CakeLine
+{
+    public abstract class HouseCakeLineSatationChainBase
+    {
+        protected HouseCakeLineSatationChainBase _nextStation;
+        public HouseCakeLineSatationChainBase SetNext(HouseCakeLineSatationChainBase station)
+        {
+            if (_nextStation == null)
+            {
+                _nextStation = station;
+            }
+            else
+            {
+                _nextStation.SetNext(station);
+            }
+
+            return this;
+        }
+    public abstract bool Act(HouseCake cake);
+    
+    }
+}
